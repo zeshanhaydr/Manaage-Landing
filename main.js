223ce2bcbfe0816bbff8368aaf679ff53d0da80a
Zeeshan Haider <zeeking295@gmail.com>
2023-08-16 04:34:51 -0700

    Footer and Slider

--- main.js
+++ main.js
@@ -9,3 +9,16 @@ navtoggle.addEventListener("click", () => {
   primaryNav.toggleAttribute("data-visible");
   primaryheader.toggleAttribute("data-overlay");
 });
+
+// Slider
+const slider = new A11YSlider(document.querySelector(".slider"), {
+  adaptiveHeight: false,
+  dots: true,
+  centreMode: true,
+  arrows: false,
+  responsive: {
+    480: {
+      dots: false, // dots enabled 1280px and up
+    },
+  },
+});
